b6530b1d5447a41dbbbffe0f58ac64930cb51d66
Najat Khoudrouf <nkhoudro@e3r3p14.1337.ma>
2023-11-22 20:31:20 +0100

    fix ex00

---
+++ ex00/Animal.cpp
@@ -0,0 +1,34 @@
+#include "Animal.hpp"
+
+Animal::Animal()
+{
+    std::cout << "default constructor Animal called" << std::endl;
+}
+
+Animal::~Animal()
+{
+    std::cout << "destructor Animal called" << std::endl;
+}
+
+Animal::Animal(const Animal &copy)
+{
+    std::cout << "copy constructor Animal called" << std::endl;
+    *this = copy;
+}
+
+Animal &Animal::operator=(const Animal &copy)
+{
+    std::cout << "assignation operator Animal called" << std::endl;
+    this->type = copy.type;
+    return (*this);
+}
+
+std::string Animal::getType() const
+{
+    return (this->type);
+}
+
+void Animal::makeSound() const
+{
+    std::cout << "Animal makes a sound" << std::endl;
+}
---
+++ ex00/WrongCat.cpp
@@ -0,0 +1,30 @@
+#include "WrongCat.hpp"
+
+WrongCat::WrongCat()
+{
+    std::cout << "WrongCat default constructor called" << std::endl;
+    this->type = "WrongCat";
+}
+
+WrongCat::~WrongCat()
+{
+    std::cout << "WrongCat destructor called" << std::endl;
+}
+
+WrongCat::WrongCat(const WrongCat &copy)
+{
+    std::cout << "WrongCat copy constructor called" << std::endl;
+    *this = copy;
+}
+
+WrongCat &WrongCat::operator=(const WrongCat &copy)
+{
+    std::cout << "WrongCat assignation operator called" << std::endl;
+    this->type = copy.type;
+    return (*this);
+}
+
+void WrongCat::makeSound() const
+{
+    std::cout << "Meow!" << std::endl;
+}
